a819e02d8ce311996850a9b5f06a658f8208e552
Boom185 <zzxzzx18555@gmail.com>
2023-01-20 05:48:14 +0700

    1

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -26,12 +26,11 @@ int main()
     cout << "Fahsai: Sawadee ka...Can you tell me your name?" << endl;
     cout << "?????: ";
     getline(cin,name);
-    cout << "Fahsai: Wow!!! " << name << " is a really cool name." << endl;
+    cout << "Fahsai: Wow!!! " << name << " is a really cool name.";
     cout << "Fahsai: I think you are an Engineering student. What is your student ID?" << endl;
     cout << name << ": ";
     cin >> sn;
-    cin.ignore();
-    gear = (sn/10000000) - 12;
+    gear = sn/10000000;
     cout << "Fahsai: I think you may be GEAR " << gear << ". I have a free movie ticket for you." << endl;
     cout << "Fahsai: Let's go to the cinema together!!!" << endl;
     cout << "Fahsai: What movie do you want to watch?" << endl;
@@ -40,9 +39,9 @@ int main()
     cout << "Fahsai: So....which day are you free to go with me?" << endl;
     cout << name << ": ";
     getline(cin,day);
-    cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching " << movie << " with you." << endl;
+    cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching " << "movie" << "with you." << endl;
     cout << name << ": ";
     getline(cin,end);
-    cout << "Fahsai: 555+ see you " << day << ". Bye Bye \\(^ ^)/";
+    cout << "Fahsai: 555+ see you" << day << "Bye Bye \(^ ^)/";
     return 0;
 }
